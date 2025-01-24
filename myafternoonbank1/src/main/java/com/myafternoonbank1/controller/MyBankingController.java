8b82351b4a1491241637e685e44a66db4a6957db
upadhyay-rajesh <upadhyay.rajesh@rediffmail.com>
2025-01-24 11:29:12 +0530

    dsd

--- myafternoonbank1/src/main/java/com/myafternoonbank1/controller/MyBankingController.java
+++ myafternoonbank1/src/main/java/com/myafternoonbank1/controller/MyBankingController.java
@@ -9,23 +9,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.myafternoonbank1.entity.Employee;
 import com.myafternoonbank1.service.MyBankingServiceInterface;
 
 @RestController
+@RequestMapping("/api/v1/employees")
 public class MyBankingController {
 	
 	@Autowired
 	private MyBankingServiceInterface mService;
 	
-	@GetMapping("displayAllEmployee")  //retrive
+	@GetMapping  //retrive
 	public List<Employee> findAllEmployee() {
 		return mService.getAllEmployeeService();
 	}
 	
-	@PostMapping("createProfile") //create
+	@PostMapping //create
 	public String register(@RequestBody Employee emp) { //using @RequestBody API will take data from client
 		
 		Employee emp1 = mService.createProfileService(emp);
@@ -33,13 +35,13 @@ public class MyBankingController {
 		return "registration success "+emp.getName()+"  "+emp.getPassword()+"  "+emp.getEmail()+"  "+emp.getAddress();
 	}
 	
-	@PutMapping("editProfile/{uid}") //update
+	@PutMapping("/{uid}") //update
 	public Employee update(@PathVariable("uid") String email,@RequestBody Employee emp) {
 		emp.setEmail(email);
 		return mService.updateProfileService(emp);
 	}
 	
-	@DeleteMapping("deleteProfile/{uid}") //delete
+	@DeleteMapping("/{uid}") //delete
 	public String remove(@PathVariable("uid") String email) {
 		return mService.deleteProfileService(email);
 	}
